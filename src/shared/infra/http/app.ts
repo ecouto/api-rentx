36d83177b0f2c616180234f9675b395b5fc94f40
Erick Couto <erick.soares@ibm.com>
2021-06-29 13:38:39 -0300

    sentry manage error

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -16,7 +16,9 @@ import swaggerFile from '../../../swagger.json';
 import { router } from '@shared/infra/http/routes';
 import upload from "@config/upload";
 import cors from "cors";
-import rateLimiter from '@shared/infra/http/middlewares/rateLimiter'
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 
 createConnection();
@@ -24,9 +26,22 @@ createConnection();
 
 const app = express();
 
-app.use(rateLimiter)
+app.use(rateLimiter);
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
+
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
@@ -36,6 +51,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(cors());
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
